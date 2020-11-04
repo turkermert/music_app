bb5dc84e22da399f242872ff5dd4cc7eee9bc634
turkermert <tmert0697@gmail.com>
2020-11-04 20:41:19 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+import axios from "axios";
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
+class App extends React.Component {
+  
+  state = {
+
+  }
+
+  render(){
+    return (
+      <div className="App">
+  <button onClick={this.handleOnPostRequest}>sa</button>
+      </div>
+    );
+  }
+  handleOnPostRequest = (event) => {
+    event.preventDefault();
+    const res = axios.post("https://musicdb.jobs.otsimo.com/createClient",{
+        "email": "deneme@gmail.com"
+      },{
+        headers:{ "Content-Type": "application/json"}
+      }).then(res => res.json()).catch(err =>console.log(err))
+  }
+}
+
+export default App;
