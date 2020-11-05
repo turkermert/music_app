2dcd5dc4a2938256133dc29eeee1f686039a9585
metehan akdag <mthnakdag@gmail.com>
2020-11-05 10:53:00 +0300

    Adding Create new User

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
 let User = require("../models/user.model");
 
-
-router.route("/getMail").get((req, res) => {
+// GET A USER WITH EMAIL
+router.route("/mail").get((req, res) => {
   const email = req.body.email;
-  User.find({usermail:email}).then(users => res.json(users))
+  User.find({usermail:email}).then(users => res.json(users)).catch(err => res.status(400).json("Error: "+ err))
 })
+
+// CREATE A NEW USER
+router.route("/add").post((req, res) => {
+    const usermail = req.body.usermail;
+    const username = req.body.username;
+    const userpassword = req.body.password;
+    const token_at= req.body.created_time;
+    
+    const newUser = new User({usermail,username,userpassword,token_at});
+    newUser.save().then(() => res.json("Created new Account Thank you "+ username)).catch(err => res.status(400).json("error: " + err))
+});
