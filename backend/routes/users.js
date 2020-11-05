909e36aff591ce8cd7dd95dab8839b40b02b77ea
metehan akdag <mthnakdag@gmail.com>
2020-11-05 11:00:07 +0300

    get a user with username

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,9 +2,15 @@ const router = require("express").Router();
 let User = require("../models/user.model");
 
 // GET A USER WITH EMAIL
-router.route("/mail").get((req, res) => {
+router.route("/getbymail").get((req, res) => {
   const email = req.body.email;
   User.find({usermail:email}).then(users => res.json(users)).catch(err => res.status(400).json("Error: "+ err))
+})
+
+// GET A USER WITH USERNAME
+router.route("/getbyname").get((req, res) => {
+  const username = req.body.username;
+  User.find({username:username}).then(users => res.json(users)).catch(err => res.status(400).json("Error: "+ err))
 })
 
 // CREATE A NEW USER
